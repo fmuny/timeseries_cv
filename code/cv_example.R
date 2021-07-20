cb510d4b6343d95dd4b8987f7dfef65840599cd9
munyf <fabian.muny@gmx.de>
2021-07-20 11:34:52 +0200

    adjust url in example

--- code/cv_example.R
+++ code/cv_example.R
@@ -7,7 +7,7 @@ require(devtools)
 data("economics")
 
 # Load functions from github
-source_url()
+source_url("https://raw.githubusercontent.com/fmuny/timeseries_cv/master/code/cv_functions.R")
 
 # Compute first differences and plot
 df_ts <- diff(ts(economics[,2:6], start = c(1967, 7), frequency = 12))
@@ -17,7 +17,7 @@ plot(df_ts)
 df_train <- window(df_ts, start = c(1990, 1), end = c(2000, 12))
 df_test  <- window(df_ts, start = c(2001, 1), end = c(2005, 12))
 
-# 1.) Timeslice
+# 1.) How to use Timeslice
 # Prepare indices
 fitControl <- trainControl('timeslice', initialWindow = 12, horizon = 12)
 # Run ML model
@@ -25,10 +25,10 @@ set.seed(156)
 model_ts <- train(pce ~., data = df_train, method = "rf", trControl = fitControl)
 # Predict on test set
 pred_ts <- predict(model_ts, newdata = df_test[,-1])
-#Compute performance measures 
+# Compute performance measures 
 perf_ts <- postResample(pred = pred_ts, obs = df_test[,1])
 
-# 2.) hv-block cross-validation
+# 2.) How to use hv-block cross-validation
 # Prepare indices
 hv <- hv_block(df_train, v_before=12, v_after=12, gap_before=12, gap_after=12)
 fitControl <- trainControl("cv", index = hv[['training']], 
@@ -38,10 +38,10 @@ set.seed(156)
 model_hv <- train(pce ~., data = df_train, method = "rf", trControl = fitControl)
 # Predict on test set
 pred_hv <- predict(model_hv, newdata = df_test[,-1])
-#Compute performance measures 
+# Compute performance measures 
 perf_hv <- postResample(pred = pred_hv, obs = df_test[,1])
 
-# 3.) Simplified block cross-validation
+# 3.) How to use simplified block cross-validation
 # Prepare indices
 simp <- simplified_block(df_train, n_splits=5, gap_before=12, gap_after=12)
 fitControl <- trainControl("cv", index = simp[['training']], 
@@ -51,10 +51,10 @@ set.seed(156)
 model_si <- train(pce ~., data = df_train, method = "rf", trControl = fitControl)
 # Predict on test set
 pred_si <- predict(model_si, newdata = df_test[,-1])
-#Compute performance measures 
+# Compute performance measures 
 perf_si <- postResample(pred = pred_si, obs = df_test[,1])['RMSE']
 
-# Print results
+# 4.) Compare results
 data.frame(In_sample_RMSE = c(round(min(model_ts$results$RMSE),3),
                               round(min(model_hv$results$RMSE),3),
                               round(min(model_si$results$RMSE),3)),
